6ee293fb8079520e02297d71171fd3582a4b08e4
Phalatena <mphalatena@gmail.com>
2023-03-27 09:30:23 -0700

    Task

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include "main.h"
-void print_buffer(char buffer[], int *buff_ind);
 
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
@@ -27,7 +27,6 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -52,6 +51,7 @@ int _printf(const char *format, ...)
 
 	return (printed_chars);
 }
+
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
